cef1a920e104e84a902653964275f7e1d3275fa8
David Chan <cfcdavidchan@gmail.com>
2017-04-11 03:21:48 +0800

    lesson 2 started

---
+++ Making_a_Web_Server/webserver.py
@@ -0,0 +1,31 @@
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+class webServerHandler(BaseHTTPRequestHandler):
+	def do_GET(self):
+		try:
+			if self.path.endswith("/hello"):
+				self.send_response(200)
+				self.send_header('Content-type', 'text/html')
+				self.end_headers()
+				message = ""
+				message += "<html><body>Hello!\n</body></html>"
+				message += "<html><body>\nFuck you!</body></html>"
+				self.wfile.write(message)
+				print (message)
+				return
+		except IOError:
+			self.send_error(404, 'File Not Found: %s' % self.path)
+
+def main():
+	try:
+		port = 1099 
+		server = HTTPServer(('', port), webServerHandler)
+		print ("Web Server running on port %s"  % port)
+		server.serve_forever()
+	except KeyboardInterrupt:
+		print (" ^C entered, stopping web server....")
+		server.socket.close()
+		server.shutdown()
+
+if __name__ == '__main__':
+	main()
